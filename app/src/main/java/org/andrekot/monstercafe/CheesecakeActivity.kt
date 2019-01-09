d20498a593cfc9a9b7a89bf22851c3a525b7a8bb
Andrew Martynets <andrekotstudio@gmail.com>
2019-01-09 13:34:09 +0300

    second commit to rebase

--- app/src/main/java/org/andrekot/monstercafe/CheesecakeActivity.kt
+++ app/src/main/java/org/andrekot/monstercafe/CheesecakeActivity.kt
@@ -53,6 +53,7 @@ class CheesecakeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListen
         Toast.makeText(this, "lastPos = $lastPos pos = $position", Toast.LENGTH_LONG).show()
         //secondCardAdapter.notifyDataSetChanged()
         //new try rebase first commit
+        //new try rebase second commit
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
